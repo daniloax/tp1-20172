3c0a430fdda5a6702ec1c0097b70149d70f0dbf8
Danilo Alves Xavier <daniloax@.unb.br>
2017-09-12 15:18:05 -0300

    option

--- ATM/src/main/scala/br/unb/cic/poo/atm/Account.scala
+++ ATM/src/main/scala/br/unb/cic/poo/atm/Account.scala
@@ -1,5 +1,7 @@
 package br.unb.cic.poo.atm
 
+class InvalidAmountException extends Exception
+
 /**
   * An account class "abstraction"
   * @param name the owner ot the account
@@ -20,14 +22,22 @@ class Account(private val name: String, protected var balance : Double = 0.0) {
     balance = balance + amount
   }
 
-  def withdraw(amount : Double) = {
-    require(amount > 0 && amount <= balance)
-    balance = balance - amount
+  def withdraw(amount : Double) : Option[Double] = {
+    if(amount > 0 && amount <= balance) {
+      balance = balance - amount
+      return Some(balance)
+    }
+    else {
+      None
+    }
   }
 
-  def transferAmount(c: Account, amount: Double): Unit = {
-    this.withdraw(amount)
-    c.deposit(amount)
+  def transferAmount(c: Account, amount: Double): Option[Double] = {
+    this.withdraw(amount) match {
+      case None     => None
+      case Some(v)  => c.deposit(amount); return Some(v)
+    }
+
   }
 
   /**
--- ATM/src/test/scala/br/unb/cic/poo/atm/AccountTest.scala
+++ ATM/src/test/scala/br/unb/cic/poo/atm/AccountTest.scala
@@ -1,7 +1,20 @@
 package br.unb.cic.poo.atm
 
+import java.sql.SQLException
+
 import org.scalatest.FunSuite
 
 class AccountTest extends FunSuite {
+
+  test("valor de saque invalido") {
+    val account = new Account("abc", 10)
+
+    val balance =   account.withdraw(20)
+    balance match {
+      case None => succeed
+      case Some(v) => fail("nao esperando um valor")
+    }
+
+  }
 
 }
